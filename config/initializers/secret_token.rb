be3b94c17ade8bf8b118c0fb8d4717cfad0d8986
ObaidMalik <obaid_malik@external.mckinsey.com>
2012-09-08 00:58:26 +0530

    Base Rails Setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Riyaz::Application.config.secret_token = '8ecac17d55eaa7d28309a98ba684f4c0defa689d9c5a283449ca3df694d5a51f2c519f183b0ae0af087e2315620294ed352aebcbd278126ef0043a34eb05aad4'
